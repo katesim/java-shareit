ea3fe546a40340f614f51ea77492c0783a05708c
katesim <sebastia256@gmail.com>
2023-01-08 17:07:03 -0800

    add item repository tests

--- src/test/java/ru/practicum/shareit/item/ItemRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/item/ItemRepositoryTest.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.item;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +7,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.transaction.AfterTransaction;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.UserRepository;
 import ru.practicum.shareit.user.model.User;
@@ -25,33 +25,76 @@ public class ItemRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
-
     private final Pageable pageable = PageRequest.of(0, 10);
 
-    private final User owner = new User(1L, "user", "user@user.com");
-
-    private final Item item = new Item(1L, "Дрель",
-            "Простая дрель", Boolean.TRUE, owner.getId(), null);
-
-    private final Item item2 = new Item(2L, "Отвертка",
-            "Аккумуляторная отвертка", Boolean.TRUE, owner.getId(), null);
+    private User owner;
+    private Item item1;
+    private Item item2;
+    private Item item3;
+    private Item item4;
 
     @BeforeEach
-    void setUp() {
-        userRepository.save(owner);
-        itemRepository.save(item);
-        itemRepository.save(item2);
+    void fillData() {
+        owner = new User();
+        owner.setName("user");
+        owner.setEmail("user@user.com");
+        em.persist(owner);
+
+        item1 = new Item();
+        item1.setName("Дрель");
+        item1.setDescription("Простая дрель");
+        item1.setAvailable(true);
+        item1.setOwnerId(owner.getId());
+
+        item2 = new Item();
+        item2.setName("Отвертка");
+        item2.setDescription("Крестовая отвертка");
+        item2.setAvailable(true);
+        item2.setOwnerId(owner.getId());
+
+        item3 = new Item();
+        item3.setName("Бензопила");
+        item3.setDescription("Дружба");
+        item3.setAvailable(true);
+        item3.setOwnerId(owner.getId());
+
+        item4 = new Item();
+        item4.setName("Перфоратор");
+        item4.setDescription("Перфоратор");
+        item4.setAvailable(false);
+        item4.setOwnerId(owner.getId());
+
+        em.persist(item1);
+        em.persist(item2);
+        em.persist(item3);
+        em.persist(item4);
     }
 
-    @AfterEach
-    void resetSetUp() {
-        em.clear();
+    @AfterTransaction
+    public void showCountAfterTransaction() {
+        System.out.println("Item count after tx: " + itemRepository.count());
     }
 
     @Test
-    void searchTest() {
-        List<Item> items = itemRepository.search("%Дрель%", pageable).toList();
-        assertEquals(1, items.size());
-        assertEquals(item, items.get(0));
+    void testSearchInName() {
+        List<Item> result = itemRepository.search("Дрель", pageable).toList();
+
+        assertEquals(1, result.size());
+        assertEquals(item1, result.get(0));
+    }
+
+    @Test
+    void testSearchInDescription() {
+        List<Item> result = itemRepository.search("Дружба", pageable).toList();
+
+        assertEquals(1, result.size());
+        assertEquals(item3, result.get(0));
+    }
+
+    @Test
+    void testSearchAndSkipNotAvailable() {
+        List<Item> result = itemRepository.search("Перфоратор", pageable).toList();
+
+        assertEquals(0, result.size());
     }
 }
