4ee0ee237db03cf87121a8bc4caa08d78ceeb977
katesim <sebastia256@gmail.com>
2023-01-10 21:09:12 -0800

    add booking controller tests

--- src/main/java/ru/practicum/shareit/booking/dto/BookingRequestDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingRequestDto.java
@@ -1,10 +1,12 @@
 package ru.practicum.shareit.booking.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import ru.practicum.shareit.booking.Status;
 
 @Data
+@Builder
 @AllArgsConstructor
 public class BookingRequestDto {
     private long id;
--- src/test/java/ru/practicum/shareit/booking/BookingTestUtils.java
+++ src/test/java/ru/practicum/shareit/booking/BookingTestUtils.java
@@ -1,6 +1,15 @@
 package ru.practicum.shareit.booking;
 
+import com.jayway.jsonpath.JsonPath;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
+
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 
 public class BookingTestUtils {
@@ -13,5 +22,41 @@ public class BookingTestUtils {
 
     public static Booking getDefaultBooking() {
         return new Booking(BOOKING_ID, BOOKING_START, BOOKING_END, ITEM_ID, USER_ID, Status.APPROVED);
+    }
+
+    public static List<Booking> generateBookings(final int count) {
+        List<Booking> bookings = new ArrayList<>();
+
+        for (long i = 1; i <= count; i++) {
+            final Booking booking = new Booking(i, BOOKING_START, BOOKING_END, ITEM_ID, USER_ID, Status.APPROVED);
+            bookings.add(booking);
+        }
+
+        return bookings;
+    }
+
+    public static void assertBookingAtIndex(
+            final String json, final List<Booking> bookings, final User user, final Item item, final int index) {
+
+        assertThat(JsonPath.read(json, "$[" + index + "].id"),
+                is(bookings.get(index).getId()));
+        assertThat(JsonPath.read(json, "$[" + index + "].start"),
+                is(bookings.get(index).getStart().toString()));
+        assertThat(JsonPath.read(json, "$[" + index + "].end"),
+                is(bookings.get(index).getEnd().toString()));
+        assertThat(JsonPath.read(json, "$[" + index + "].status"),
+                is(bookings.get(index).getStatus().name()));
+
+        assertThat(JsonPath.read(json, "$[" + index + "].item.id"),
+                is(item.getId()));
+        assertThat(JsonPath.read(json, "$[" + index + "].item.name"),
+                is(item.getName()));
+        assertThat(JsonPath.read(json, "$[" + index + "].item.description"),
+                is(item.getDescription()));
+
+        assertThat(JsonPath.read(json, "$[" + index + "].booker.id"),
+                is(user.getId()));
+        assertThat(JsonPath.read(json, "$[" + index + "].booker.name"),
+                is(user.getName()));
     }
 }
